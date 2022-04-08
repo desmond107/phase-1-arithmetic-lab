7df02a21741a92a000c36f60b99df43cb4984f8c
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-08 16:43:23 +0300

    task complet

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your code here
-var num1 = 31;
-var num2 = 2;
-var num3 = 12;
-var num4 = 8;
+let num1 = 31;
+let num2 = 2;
+let num3 = 12;
+let num4 = 8;
 
-var multiply = num1 * num2
+let multiply = num1 * num2
 
-var random = Math.floor(Math.random() * 10) + 1;
+let random = Math.floor(Math.random() * 10) + 1;
 
-var mod = num3 % num4;
+let  mod = num3 % num4;
 
-var max = Math.max(1, 2, 5, 8, 10, 12, 15, 20);
+let  max = Math.max(1, 2, 5, 8, 10, 12, 15, 20);
